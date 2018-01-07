3e1db368a98250dbb308a90614a93e2dad8b5c34
desmond beramendi <desmond7187@gmail.com>
2018-01-07 22:09:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nestedTarget(target) {
 }
 
 function increaseRankBy(n) {
-  const lis =  document.getElementById('main').querySelectorAll('ul.ranked-list li');
+  const lis =  document.querySelectorAll('ul.ranked-list li');
   
   for(let i = 0; i < lis.length; i++) {
     lis[i].innerHTML = parseInt((i + n));
